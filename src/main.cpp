43244e333f2ececc4d0b4fedf4c2f96405da172b
AV-Coding <villasenor.ariel@gmail.com>
2020-10-10 11:59:04 -0700

    Another push

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 // Your First C++ Program
 
 #include <iostream>
+#include <fstream>
+
 using namespace std;
-int main() {
-    cout << "Hello World!";
+
+int main(int argc, const char * argv[]) {
+    for (int i = 0; i < argc; ++i) 
+        cout << i << ":" << argv[i] << "\n";
+    ofstream myfile;
+    myfile.open(argv[1]);
+    cout << "Hello World!" << "\n";
     return 0;
 }
