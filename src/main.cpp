6eca64f525cbe4869a89887653667a37341353fa
AV-Coding <villasenor.ariel@gmail.com>
2020-10-10 12:03:53 -0700

    main.cpp checks if file exists

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,14 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     for (int i = 0; i < argc; ++i) 
         cout << i << ":" << argv[i] << "\n";
-    ofstream myfile;
+    ifstream myfile;
     myfile.open(argv[1]);
+    if (myfile){
+        cout << "file exists" << "\n";
+    }else{
+        cout << "file doesn't exist" << "\n";
+        return 0;
+    }
     cout << "Hello World!" << "\n";
     return 0;
 }
