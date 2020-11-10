3ade0925a046203d486a3bc6df459ba287c75fe6
AV-Coding <villasenor.ariel@gmail.com>
2020-11-09 17:10:04 -0700

    Deleted unecessary files

--- src/verilogFile.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-module verilogFile(b, a, c);
-	input [31:0] a;
-	input [15:0] b;
-	output [7:0] c;
-	wire [7:0] cwire;
-	SADD #(.WIDTH(8))line1(a[7:0],b[7:0],cwire);
-	REG #(.WIDTH(8))line2(CLK,RST,cwire,c);
-endmodule
